82c85c00fe290a4123343c712608869a160d0256
shaine <muswereshaine@gmail.com>
2021-05-18 12:01:37 +0200

    Oware and Oware.Tests

--- Oware.Tests/UnitTest1.cs
+++ Oware.Tests/UnitTest1.cs
@@ -10,9 +10,19 @@ namespace Oware.Tests
         }
 
         [Test]
-        public void Test1()
+        public void WhenResetHouseSeedsShouldBeFour()
         {
-            Assert.Pass();
+            //ARRANGE
+            //nothing here
+            //ACT
+            House h = new House(0, 0);
+            h.ResetHouse();
+            //ASSERT
+            Assert.AreEqual(4, h.GetCount(), "Reset  houses must have 4 seeds in them.");
         }
     }
 }
+
+// The Player class depends on ScoreHouse. Use a mock to test
+// AddSeedToScoreHouse and check that it does, in fact, call ScoreHouse’s 
+//AddSeed method.
